b1e3de927ff1e387392dca67cd7fad14b55a8e10
giftnkz6 <giftnkz6@gmail.com>
2023-02-27 17:00:32 +0200

    Added .env

--- config/index.js
+++ config/index.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const {createPool} = require('mysql')
 // Create connection variable
 let connection = createPool({
-    host: process.env.hostName,
-    user: process.env.userName,
-    password: process.env.userPass,
-    port: process.env.portDB,
-    database: process.env.dbName,
-    multiStatements: true
-  });
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password: process.env.USERPASS,
+  database: process.env.DB_NAME,
+  port: process.env.PORT_DB,
+  multipleStatements: true
+});
 
-  module.exports = connection;
+
+module.exports = connection;
